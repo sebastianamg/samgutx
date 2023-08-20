20997f2b27c9e6770ede99bb652570293cd68e92
sebastianamg <smoraga@gmail.com>
2023-08-19 21:46:48 -0300

    Development of a Basic Memory and Time Profiler Fixes #2

--- samg/profiler.hpp
+++ samg/profiler.hpp
@@ -1,7 +1,27 @@
 #include <chrono>
-
+#include <map>
+#include <string>
 namespace samg {
     namespace profiler {
-        // TODO ...
+        template <class TimeT  = std::chrono::nanoseconds, class ClockT = std::chrono::steady_clock>
+        class Profiler {
+            public:
+                enum ProfileEntry {
+                        EXECUTION_TIME,
+                        MEMORY_USAGE
+                };
+                template <class F, class ...Args>
+                auto measure_time(F&& func, Args&&... args) {
+                    auto start = ClockT::now();
+                    auto ans = std::invoke(std::forward<F>(func), std::forward<Args>(args)...);
+                    this->profile[ProfileEntry::EXECUTION_TIME] = std::chrono::duration_cast<TimeT>(ClockT::now()-start).count();
+                    return ans;
+                }
+                std::map<ProfileEntry,double> get_profile() {
+                    return this->profile;
+                }
+            private:
+                std::map<ProfileEntry,double> profile;
+        };
     };
 };

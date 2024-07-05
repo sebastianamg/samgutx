7da93a9fe4aad40568a8c4c216abec0212568377
sebastianamg <smoraga@gmail.com>
2024-07-05 15:39:45 +0700

    Updates.

--- samg/matutx-graph.hpp
+++ samg/matutx-graph.hpp
@@ -8,7 +8,8 @@
 // #include <webgraph-graal/webgraph-3.6.11.h>
 #include <boost/shared_ptr.hpp>
 #include <boost/tuple/tuple.hpp>
-#include <webgraph/webgraph/webgraph.hpp>
+// #include <webgraph/webgraph/webgraph.hpp>
+#include <webgraph/webgraph/boost/integration.hpp> 
 
 
 /**
@@ -62,27 +63,31 @@ namespace samg {
                     std::float_t gauss_sigma;
                     std::uint64_t clustering;
                     std::float_t clustering_distance_error;
-                    
+
+                    // boost::graph_traits<webgraph::bv_graph::graph>::vertex_iterator v, v_end;
+                    boost::graph_traits<webgraph::bv_graph::graph>::edge_iterator e, e_end;
+
+
                     boost::shared_ptr<webgraph::bv_graph::graph> graph;
-                    webgraph::bv_graph::graph::node_iterator entries_iter, entries_end;
-                    std::vector<int> successors;
-                    std::size_t successors_index;
+                    // webgraph::bv_graph::graph::node_iterator entries_iter, entries_end;
+                    // std::vector<int> successors;
+                    // std::size_t successors_index;
                     // graal_isolate_t *isolate = NULL;
                     // graal_isolatethread_t *thread = NULL;
 
-                    void _update_() {
-                        while( this->successors_index >= this->successors.size() && this->entries_iter != this->entries_end ) {
-                            this->successors = successor_vector( this->entries_iter );
-                            ++(this->entries_iter);
-                            this->successors_index = 0;
-                        }
-                    }
+                    // void _update_() {
+                    //     while( this->successors_index >= this->successors.size() && this->entries_iter != this->entries_end ) {
+                    //         this->successors = webgraph::bv_graph::successor_vector( this->entries_iter );
+                    //         ++(this->entries_iter);
+                    //         this->successors_index = 0;
+                    //     }
+                    // }
 
                 public:
                     GraphReader(std::string file_name) :
                         Reader(file_name),
-                        graph (webgraph::bv_graph::graph::load_offline( samg::utils::get_file_basename(file_name) )),
-                        successors_index(0ZU)
+                        graph (webgraph::bv_graph::graph::load_offline( samg::utils::get_file_basename(file_name) ))//,
+                        // successors_index(0ZU)
                     {
                         this->max_per_dimension = std::vector<std::uint64_t>();
                         std::string properties_file = samg::utils::change_extension(file_name,"properties");
@@ -106,7 +111,8 @@ namespace samg {
                             throw std::runtime_error("Graph properties file \""+properties_file+"\" not available.");
                         }
 
-                        std::tie(this->entries_iter, this->entries_end) = this->graph->get_node_iterator( 0 );
+                        // std::tie(this->entries_iter, this->entries_end) = this->graph->get_node_iterator( 0 );
+                        std::tie( this->e, this->e_end ) = boost::edges( *(this->graph) );
                     }
                     // ~GraphReader() {
                     //     graal_tear_down_isolate(this->thread);
@@ -122,7 +128,8 @@ namespace samg {
                         return this->number_of_entries;
                     }
                     const bool has_next() override {
-                        return this->entries_iter != this->entries_end || this->successors_index < this->successors.size();
+                        // return this->entries_iter != this->entries_end || this->successors_index < this->successors.size();
+                        return this->e != this->e_end;
                     }
 
                     const std::uint64_t get_matrix_side_size() const override {
@@ -155,12 +162,13 @@ namespace samg {
                     }
 
                     std::vector<std::uint64_t> next() override {
-                        this->_update_();
+                        // this->_update_();
                         if( this->has_next() ) {
                             // Assuming 2-dimensional matrix:
                             std::vector<std::uint64_t> entries = std::vector<std::uint64_t>(this->max_per_dimension.size());
-                            entries[0] = *(this->entries_iter);
-                            entries[1] = successors[this->successors_index++];
+                            entries[0] = this->e->first;//*(this->entries_iter);
+                            entries[1] = this->e->second;//successors[this->successors_index++];
+                            ++(this->e);
                             return entries;
                         }
                         throw std::runtime_error("No more entries.");
@@ -168,6 +176,127 @@ namespace samg {
             };
 
         };
+
+
+        //     class GraphReader : public Reader {
+        //         private:
+        //             std::vector<std::uint64_t> max_per_dimension;
+        //             std::uint64_t number_of_entries;
+        //             std::uint64_t matrix_side_size;
+        //             std::uint64_t matrix_size;
+        //             std::float_t matrix_expected_density;
+        //             std::float_t matrix_actual_density;
+        //             std::string matrix_distribution;
+        //             std::float_t gauss_mu;
+        //             std::float_t gauss_sigma;
+        //             std::uint64_t clustering;
+        //             std::float_t clustering_distance_error;
+                    
+        //             boost::shared_ptr<webgraph::bv_graph::graph> graph;
+        //             webgraph::bv_graph::graph::node_iterator entries_iter, entries_end;
+        //             std::vector<int> successors;
+        //             std::size_t successors_index;
+        //             // graal_isolate_t *isolate = NULL;
+        //             // graal_isolatethread_t *thread = NULL;
+
+        //             void _update_() {
+        //                 while( this->successors_index >= this->successors.size() && this->entries_iter != this->entries_end ) {
+        //                     this->successors = webgraph::bv_graph::successor_vector( this->entries_iter );
+        //                     ++(this->entries_iter);
+        //                     this->successors_index = 0;
+        //                 }
+        //             }
+
+        //         public:
+        //             GraphReader(std::string file_name) :
+        //                 Reader(file_name),
+        //                 graph (webgraph::bv_graph::graph::load_offline( samg::utils::get_file_basename(file_name) )),
+        //                 successors_index(0ZU)
+        //             {
+        //                 this->max_per_dimension = std::vector<std::uint64_t>();
+        //                 std::string properties_file = samg::utils::change_extension(file_name,"properties");
+        //                 std::string defaults = samg::utils::read_from_file( properties_file.data() );
+        //                 std::map<std::string, std::string> properties;
+        //                 if( cpp_properties::parse(defaults.begin(),defaults.end(),properties) ) {
+        //                     std::uint64_t nodes = std::atoll( properties["nodes"].data() );
+        //                     std::uint64_t arcs = std::atoll( properties["arcs"].data() );
+        //                     this->max_per_dimension = { nodes , nodes };
+        //                     this->number_of_entries = arcs;
+        //                     this->matrix_side_size = nodes;
+        //                     this->matrix_size = nodes*nodes;
+        //                     this->matrix_expected_density = (double) arcs / (double) this->matrix_size;
+        //                     this->matrix_actual_density = (double) arcs / (double) this->matrix_size;
+        //                     this->matrix_distribution = "null";
+        //                     std::float_t gauss_mu = -1.0f;
+        //                     std::float_t gauss_sigma = -1.0f;
+        //                     std::uint64_t clustering = 0ull;
+        //                     std::float_t clustering_distance_error = -1.0f;
+        //                 } else {
+        //                     throw std::runtime_error("Graph properties file \""+properties_file+"\" not available.");
+        //                 }
+
+        //                 std::tie(this->entries_iter, this->entries_end) = this->graph->get_node_iterator( 0 );
+        //             }
+        //             // ~GraphReader() {
+        //             //     graal_tear_down_isolate(this->thread);
+        //             // }
+
+        //             const std::size_t get_number_of_dimensions() const override {
+        //                 return this->max_per_dimension.size();
+        //             }
+        //             const std::vector<std::uint64_t> get_max_per_dimension() const override {
+        //                 return this->max_per_dimension;
+        //             }
+        //             const std::uint64_t get_number_of_entries() const override {
+        //                 return this->number_of_entries;
+        //             }
+        //             const bool has_next() override {
+        //                 return this->entries_iter != this->entries_end || this->successors_index < this->successors.size();
+        //             }
+
+        //             const std::uint64_t get_matrix_side_size() const override {
+        //                 // std::cout << "GraphReader>get_matrix_side_size> (1)" << std::endl;
+        //                 return this->matrix_side_size;
+        //             }
+        //             const std::uint64_t get_matrix_size() const override {
+        //                 return this->matrix_size;
+        //             }
+        //             const std::float_t get_matrix_expected_density() const override {
+        //                 return this->matrix_expected_density;
+        //             }
+        //             const std::float_t get_matrix_actual_density() const override {
+        //                 return this->matrix_actual_density;
+        //             }
+        //             const std::string get_matrix_distribution() const override {
+        //                 return this->matrix_distribution;
+        //             }
+        //             const std::float_t get_gauss_mu() const override {
+        //                 return this->gauss_mu;
+        //             }
+        //             const std::float_t get_gauss_sigma() const override {
+        //                 return this->gauss_sigma;
+        //             }
+        //             const std::uint64_t get_clustering() const override {
+        //                 return this->clustering;
+        //             }
+        //             const std::float_t get_clustering_distance_error() const override {
+        //                 return this->clustering_distance_error;
+        //             }
+
+        //             std::vector<std::uint64_t> next() override {
+        //                 this->_update_();
+        //                 if( this->has_next() ) {
+        //                     // Assuming 2-dimensional matrix:
+        //                     std::vector<std::uint64_t> entries = std::vector<std::uint64_t>(this->max_per_dimension.size());
+        //                     entries[0] = *(this->entries_iter);
+        //                     entries[1] = successors[this->successors_index++];
+        //                     return entries;
+        //                 }
+        //                 throw std::runtime_error("No more entries.");
+        //             }
+        //     };
+
+        // };
         //     class GraphReader : public Reader {
         //         private:
         //             std::vector<std::uint64_t> max_per_dimension;

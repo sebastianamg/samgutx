fb5a970c8834c573d665cbd79605039af13fdc49
sebastianamg <smoraga@gmail.com>
2026-07-14 10:54:15 +0700

    src/profiler: Improvements on the profiler logic.

--- samg/profiler.hpp
+++ samg/profiler.hpp
@@ -1,11 +1,11 @@
 #pragma once
 #include <fcntl.h>
 #include <unistd.h>
+#include <fstream>
 #include <chrono>
 #include <map>
 #include <cmath>
 #include <string>
-#include <string.h>
 #include <functional>
 
 namespace samg {
@@ -13,141 +13,120 @@ namespace samg {
 
         /**
          * @brief This class allows measure exection time and/or memory usage of a given function.
-         * 
-         * @tparam TimeT 
-         * @tparam ClockT 
          */
-        template <class TimeT  = std::chrono::nanoseconds, class ClockT = std::chrono::steady_clock>
+        template <typename TimeT = std::chrono::nanoseconds, typename ClockT = std::chrono::high_resolution_clock>
         class Profiler {
             public:
-                static const std::string EXECUTION_TIME;
-                static const std::string MEMORY_USAGE;
-                static const std::string INITIAL_MEMORY;
-                static const std::string FINAL_MEMORY;
+                /**
+                 * @brief A structure to hold the results of a profiling run.
+                 * It is designed to be simple and extensible for future metrics.
+                 */
+                struct ProfileData {
+                    std::uint64_t time_ns = 0;
+                    std::uint64_t mem_initial_kb = 0;
+                    std::uint64_t mem_final_kb = 0;
+                    std::uint64_t mem_delta_kb = 0;
+                    // Extensible map for future hardware counters (Stage 2)
+                    std::map<std::string, std::uint64_t> counters;
+                };
 
                 /**
-                 * @brief This function measures execution time of a given function. 
-                 * 
-                 * @tparam R 
-                 * @tparam F 
-                 * @tparam Args 
-                 * @param func 
-                 * @param args 
-                 * @return R 
+                 * @brief Measures the execution time of a given function.
+                 * @tparam R The return type of the function to profile.
+                 * @tparam F The function type.
+                 * @tparam Args The argument types for the function.
+                 * @param func The function to execute and profile.
+                 * @param args The arguments to pass to the function.
+                 * @return A pair containing the function's return value and the profile data.
                  */
                 template <typename R, class F, class ...Args>
-                R measure_time(F&& func, Args&&... args) {
+                std::pair<R, ProfileData> measure_time(F&& func, Args&&... args) {
+                    ProfileData data;
                     auto start = ClockT::now();
                     R ans = std::invoke(std::forward<F>(func), std::forward<Args>(args)...);
-                    this->profile[Profiler::EXECUTION_TIME] = std::to_string( std::chrono::duration_cast<TimeT>(ClockT::now()-start).count() );
-                    return ans;
+                    auto end = ClockT::now();
+                    data.time_ns = std::chrono::duration_cast<TimeT>(end - start).count();
+                    return {ans, data};
                 }
 
                 /**
-                 * @brief This function measures memory usage of a given function. 
-                 * 
-                 * @tparam R 
-                 * @tparam F 
-                 * @tparam Args 
-                 * @param func 
-                 * @param args 
-                 * @return R 
+                 * @brief Measures the memory usage of a given function.
+                 * @tparam R The return type of the function to profile.
+                 * @tparam F The function type.
+                 * @tparam Args The argument types for the function.
+                 * @param func The function to execute and profile.
+                 * @param args The arguments to pass to the function.
+                 * @return A pair containing the function's return value and the profile data.
                  */
                 template <typename R, class F, class ...Args>
-                R measure_memory(F&& func, Args&&... args) {
+                std::pair<R, ProfileData> measure_memory(F&& func, Args&&... args) {
+                    ProfileData data;
                     const pid_t this_process = getpid();
-                    std::uint64_t initial_memory = Profiler::_get_memory_usage_(this_process);
+                    data.mem_initial_kb = Profiler::_get_memory_usage_kb(this_process);
                     R ans = std::invoke(std::forward<F>(func), std::forward<Args>(args)...);
-                    std::uint64_t ending_memory = Profiler::_get_memory_usage_(this_process);
-                    this->profile[Profiler::MEMORY_USAGE] = std::to_string( ending_memory > initial_memory ?  ending_memory - initial_memory : initial_memory - ending_memory );
-                    return ans;
+                    data.mem_final_kb = Profiler::_get_memory_usage_kb(this_process);
+                    data.mem_delta_kb = (data.mem_final_kb > data.mem_initial_kb) 
+                                      ? (data.mem_final_kb - data.mem_initial_kb) 
+                                      : 0;
+                    return {ans, data};
                 }
 
                 /**
-                 * @brief This function measures both execution time and memory usage of a given function. 
-                 * 
-                 * @tparam R 
-                 * @tparam F 
-                 * @tparam Args 
-                 * @param func 
-                 * @param args 
-                 * @return R 
+                 * @brief Measures both execution time and memory usage of a given function.
+                 * @tparam R The return type of the function to profile.
+                 * @tparam F The function type.
+                 * @tparam Args The argument types for the function.
+                 * @param func The function to execute and profile.
+                 * @param args The arguments to pass to the function.
+                 * @return A pair containing the function's return value and the profile data.
                  */
                 template <typename R, class F, class ...Args>
-                R measure_all(F&& func, Args&&... args) {
+                std::pair<R, ProfileData> measure_all(F&& func, Args&&... args) {
+                    ProfileData data;
                     const pid_t this_process = getpid();
-                    std::uint64_t initial_memory = Profiler::_get_memory_usage_(this_process);
-                    this->profile[Profiler::INITIAL_MEMORY] = std::to_string( initial_memory ); 
+
+                    data.mem_initial_kb = Profiler::_get_memory_usage_kb(this_process);
+                    
                     auto start = ClockT::now();
                     R ans = std::invoke(std::forward<F>(func), std::forward<Args>(args)...);
-                    // auto ans = std::invoke(std::forward<F>(func), std::forward<Args>(args)...);
-                    this->profile[Profiler::EXECUTION_TIME] = std::to_string( std::chrono::duration_cast<TimeT>(ClockT::now()-start).count() );
-                    std::uint64_t ending_memory = Profiler::_get_memory_usage_(this_process);
-                    this->profile[Profiler::FINAL_MEMORY] = std::to_string( ending_memory );
-                    this->profile[Profiler::MEMORY_USAGE] = std::to_string( ending_memory > initial_memory ?  ending_memory - initial_memory : initial_memory - ending_memory );
-                    return ans;
-                }
+                    auto end = ClockT::now();
 
-                /**
-                 * @brief This function adds a new entry in the measurement profile. 
-                 * 
-                 * @param key 
-                 * @param entry 
-                 */
-                void add_profile_entry( std::string key, std::string entry ) {
-                    this->profile[key] = entry;
-                }
+                    data.mem_final_kb = Profiler::_get_memory_usage_kb(this_process);
+                    data.time_ns = std::chrono::duration_cast<TimeT>(end - start).count();
+                    data.mem_delta_kb = (data.mem_final_kb > data.mem_initial_kb) 
+                                      ? (data.mem_final_kb - data.mem_initial_kb) 
+                                      : 0;
 
-                /**
-                 * @brief This function returns the measurement profile. 
-                 * 
-                 * @return std::map<std::string,std::string> 
-                 */
-                std::map<std::string,std::string> get_profile() {
-                    return this->profile;
+                    return {ans, data};
                 }
             private:
-                std::map<std::string,std::string> profile;
-
                 /**
-                 * @brief This function returns the memory usage of a process. 
-                 * 
-                 * @param pid 
-                 * @return std::uint64_t 
+                 * @brief Returns the current memory usage (data + stack) of a process in kilobytes.
+                 * @param pid The process ID.
+                 * @return Memory usage in KB, or 0 on failure.
                  */
-                static std::uint64_t _get_memory_usage_(pid_t pid) {
-                    const std::size_t max_name_length = 64;
-                    int fd, data, stack;
-                    char buf[4096], status_child[max_name_length];
-                    char *vm;
-
-                    sprintf(status_child, "/proc/%d/status", pid);
-                    if ((fd = open(status_child, O_RDONLY)) < 0){
-                        return -1;
+                static std::uint64_t _get_memory_usage_kb(pid_t pid) {
+                    std::string status_path = "/proc/" + std::to_string(pid) + "/status";
+                    std::ifstream status_file(status_path);
+                    if (!status_file.is_open()) {
+                        return 0;
                     }
 
-                    data = stack = 0;
-                    if( read(fd, buf, 4095) != -1 ){
-                        buf[4095] = '\0';
-                        vm = strstr(buf, "VmData:");
-                        if (vm) {
-                            sscanf(vm, "%*s %d", &data);
+                    std::string line;
+                    std::uint64_t data = 0, stack = 0;
+                    while (std::getline(status_file, line)) {
+                        if (line.rfind("VmData:", 0) == 0) {
+                            std::stringstream ss(line);
+                            std::string key;
+                            ss >> key >> data;
+                        } else if (line.rfind("VmStk:", 0) == 0) {
+                            std::stringstream ss(line);
+                            std::string key;
+                            ss >> key >> stack;
                         }
-                        vm = strstr(buf, "VmStk:");
-                        if (vm) {
-                            sscanf(vm, "%*s %d", &stack);
-                        }
-
-                        // free(vm);
                     }
-                    close(fd);
-
                     return data + stack;
                 }
         };
-        template <class TimeT, class ClockT> const std::string Profiler<TimeT,ClockT>::EXECUTION_TIME  = "FUNCTION EXECUTION TIME [ns]";
-        template <class TimeT, class ClockT> const std::string Profiler<TimeT,ClockT>::MEMORY_USAGE    = "FUNCTION MEMORY USAGE [B]";
-        template <class TimeT, class ClockT> const std::string Profiler<TimeT,ClockT>::INITIAL_MEMORY  = "FUNCTION INITIAL MEMORY [B]";
-        template <class TimeT, class ClockT> const std::string Profiler<TimeT,ClockT>::FINAL_MEMORY    = "FUNCTION FINAL MEMORY [B]";
     };
 };
